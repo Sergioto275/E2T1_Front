e5448017172d9ddc41a0be6b0d8b49de882a69a0
afernandezi22 <afernandezi22@fpsanjorge.com>
2024-01-16 12:40:40 +0100

    Actualizada tabla de horarios + translations + correcciones langile js.

--- js/translations.js
+++ js/translations.js
@@ -7,6 +7,7 @@ const translations = {
           borrar: 'Borrar',
           salir: 'Salir',
           guardar: 'Guardar',
+          filtroGrupo: 'Selecciona un grupo'
         },
         trabajador: {
           titulo: 'Trabajador',
@@ -18,6 +19,20 @@ const translations = {
           modal:{
             editar: 'Editar trabajador',
             añadir: 'Añadir trabajador',
+          }
+        },
+        horarios: {
+          titulo: 'Horarios',
+          id: 'ID',
+          codigo: 'Código',
+          dia: 'Día',
+          fechaInicio: 'Fecha inicio',
+          fechaFinal: 'Fecha final',
+          horaInicio: 'Hora inicio',
+          horaFinal: 'Hora final',
+          modal: {
+            editar: 'Editar horario',
+            añadir: 'Añadir horario',
           }
         },
         menu: {
@@ -60,6 +75,7 @@ const translations = {
         borrar: 'Ezabatu',
         salir: 'Irten',
         guardar: 'Gorde',
+        filtroGrupo: 'Taldea aukeratu'
       },
       trabajador: {
         titulo: 'Langile',
@@ -71,6 +87,20 @@ const translations = {
         modal:{
           editar: 'Langilea editatu',
           añadir: 'Langilea sortu',
+        }
+      },
+      horarios: {
+        titulo: 'Ordutegiak',
+        id: 'ID',
+        codigo: 'Kodea',
+        dia: 'Eguna',
+        fechaInicio: 'Hasiera data',
+        fechaFinal: 'Amaiera data',
+        horaInicio: 'Hasiera ordua',
+        horaFinal: 'Amaiera ordua',
+        modal: {
+          editar: 'Ordutegia editatu',
+          añadir: 'Ordutegia sortu',
         }
       },
       menu: {
