85f7c504987748ae019e95c3b17986fbc84d1424
afernandezi22 <afernandezi22@fpsanjorge.com>
2024-01-17 10:05:15 +0100

    Corregir error que rompía toda la web

--- js/LangileScript.js
+++ js/LangileScript.js
@@ -17,7 +17,7 @@ new Vue({
       grupoFil:"",
       currentLocale: 'es',
       translations: translations,
-      environment: 'http://localhost/Erronka2/Back/talde1erronka2',
+      environment: 'https://localhost/Erronka2/Back/talde1erronka2',
     },
     methods: {
       changeEnvironment(env){
